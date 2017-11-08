d4bf70bb2252d145ac8679a6857161d17123a124
Michael Keyes <michaelkeyes@Michaels-Air.home>
2017-11-07 21:15:02 -0500

    added README.md and updates to clp

---
+++ README.md
@@ -0,0 +1,25 @@
+# Command Line Parser 
+## (Ascending Sort, Descending Sort, Reverse, Unsorted display of text file)
+
+This program is a command line parser that enables users
+to print a text file line by line.
+
+## Requirements
+
+* Python 2.7.14
+* os module
+* argparse module
+
+## How to Run
+
+1. Download clp.py into directory with textfile to be read
+2. `python clp.py textfile.txt`
+  * Use --reverse to reverse read output
+  * Use --asort for ascending sort
+  * Use --rsort for descending sort
+  * Use no option/flag for unsorted display
+
+ 
+
+
+
--- clp.py
+++ clp.py
@@ -1,17 +1,31 @@
 import argparse
+import os
 
 ## Create Parser object
-parser = argparse.ArgumentParser(description="Process some integers. ")
+parser = argparse.ArgumentParser(description="Sort by line. Reverse option")
+
+## Create 'file' positional argument
+parser.add_argument('file', metavar='FILE', type=file,
+                    help='a file to be sorted')
+
+## Create '--sort' option/flag
+parser.add_argument('--sort', dest='sort', action='store_const',
+                      const=True, help='boolean for sorting')
+
+## Create '--reverse' option/flag
+parser.add_argument('--reverse', dest='reverse', action='store_const',
+                      const=True, help='boolean set to for reverse sorting')
 
 
-## Add arguments 'integers' and '--sum'
-parser.add_argument('integers', metavar='N', type=int, nargs='+',
-                    help='an integer for the accumulator')
-
-parser.add_argument('--sum', dest='sum', action='store_const',
-                    const=sum, default=max,
-                    help='avg the integers (default: find the max)')
-
-##
+## Parse Argument Object
 args = parser.parse_args()
-print args.sum(args.integers)
+
+if(args.sort):
+    for line in sorted(args.file.readlines()):
+        print line.rstrip()
+elif(args.reverse):
+    for line in reversed(args.file.readlines()):
+        print line.rstrip()
+else:
+    for line in args.file.readlines():
+        print line.rstrip()
